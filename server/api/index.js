d2c042353be29f2dbb36db0f07daa535979ab189
fajriapril33-2505 <fajriapril33@gmail.com>
2026-06-14 16:18:02 +0700

    fix: export Hono app directly to leverage Vercel's native Hono handler

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,3 @@
-const { handle } = require("hono/vercel");
 const app = require("../dist/app.js").default;
 
-const runtime = "nodejs";
-
-module.exports = handle(app);
-module.exports.runtime = runtime;
+module.exports = app;
